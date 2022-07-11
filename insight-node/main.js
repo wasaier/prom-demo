e5bbf3dae3c80a4cde691f819e9274c0d262444f
waisai <2856116089@qq.com>
2022-07-11 23:46:33 +0800

    chore: add fe-demo

--- insight-node/main.js
+++ insight-node/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 const register = new client.Registry();
 const app = express();
+const url = require('url');
 
 // 新增默认标签，默认添加到所有的 metrics
 register.setDefaultLabels({
@@ -14,28 +15,25 @@ client.collectDefaultMetrics({ register });
 
 // 自定义 metrics
 const monitor_api_request_total = new client.Counter({
-  name: "monitor_api_request_total",
+  name: "monitor_api_req_total",
   help: "metric_help",
   labelNames: ["url"],
 });
 
 register.registerMetric(monitor_api_request_total)
 
-app.get('/home', async function (req, res) {
-  monitor_api_request_total.inc({ url: '/home' });
+app.get('*', async function (req, res, next) {
+  const { pathname } = url.parse(req.url)
+  monitor_api_request_total.inc({ url: pathname });
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 })
 
-app.get('/about', async function (req, res) {
-  monitor_api_request_total.inc({ url: '/about' });
+app.post('/report', async function (req, res, next) {
+  const { pathname } = url.parse(req.url)
+  monitor_api_request_total.inc({ url: pathname });
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 })
-
-app.get("/metrics", async function (req, res) {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
-});
 
 app.listen(8080)
